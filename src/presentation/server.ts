5611a2b4cecf4c5f8be7383eb8bfff8fc379b4e6
Jesus Joyas <jesus.joyas@globant.com>
2025-10-06 20:40:10 +0200

    Added restful api

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,29 +1,40 @@
-import express from "express";
+import express, { Router } from "express";
 import { rmSync } from "fs";
 import path from "path";
 
 interface Options {
   port: number;
   public_path?: string;
+  routes: Router;
 }
 
 export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = "public" } = options;
+    const { routes, port, public_path = "public" } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   async start() {
     // * middleware
 
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
     // * public folder
     this.app.use(express.static(this.publicPath));
 
+    // routes
+
+    this.app.use(this.routes);
+
+    // SPA
     this.app.use((req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`
